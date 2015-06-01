935eb4a1df7d6d9e5a52e649b470f10766012f83
Nick Vernij <hello@nickforall.nl>
2015-06-01 23:33:26 +0200

    minor imrpovement playermanager

--- src/org/prismarine/server/players/PlayerManager.java
+++ src/org/prismarine/server/players/PlayerManager.java
@@ -2,19 +2,32 @@ package org.prismarine.server.players;
 
 import com.google.common.collect.Maps;
 import net.minecraft.server.EntityPlayer;
+import net.minecraft.server.PlayerList;
 import org.prismarine.api.players.Player;
 
 import java.util.Map;
+import java.util.UUID;
 
 public class PlayerManager {
 
-    private Map<Player, EntityPlayer> players = Maps.newHashMap();
+    private Map<EntityPlayer, Player> players = Maps.newHashMap();
+    private PlayerList nms;
 
-    public PlayerManager(){
-
+    public PlayerManager(PlayerList nms){
+        this.nms = nms;
     }
 
     public void addPlayer(EntityPlayer p){
-        players.put(new PrismarinePlayer(p), p);
+        players.put(p, new PrismarinePlayer(p));
+    }
+
+    public Player getPlayer(UUID id){
+        EntityPlayer _p = nms.a(id);
+        return players.get(_p);
+    }
+
+    public Player getPlayer(String id){
+        EntityPlayer _p = nms.getPlayer(id);
+        return players.get(_p);
     }
 }
